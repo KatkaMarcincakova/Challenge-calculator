3cde677913b6389ba89b09fb0089a46a2c4df0b2
Katka Marcincakova <katulik137@gmail.com>
2023-01-06 10:59:00 +0100

    navbar done

--- src/navbar.js
+++ src/navbar.js
@@ -1,10 +1,12 @@
 $(window).on("scroll touchmove", function() {
-    if ($(document).scrollTop() > 100 && !$("#navbar").hasClass("scrolled-navbar")) {
+    if ($(document).scrollTop() > 100 && $("#navbar").hasClass("not-scrolled-navbar")) {
+        $("#navbar").removeClass("not-scrolled-navbar");
         $("#navbar").addClass("scrolled-navbar");
         console.log("xx");
     }
     if ($(document).scrollTop() < 100 && $("#navbar").hasClass("scrolled-navbar")) {
         $("#navbar").removeClass("scrolled-navbar");
+        $("#navbar").addClass("not-scrolled-navbar");
         console.log("ee");
     }
 })
