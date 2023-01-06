a4c31bbcdf9576a76be557c92d7fc09e54ec7481
Katka Marcincakova <katulik137@gmail.com>
2023-01-06 10:39:52 +0100

    navbar color fixed

--- src/navbar.js
+++ src/navbar.js
@@ -1,10 +1,10 @@
 $(window).on("scroll touchmove", function() {
-    if ($(document).scrollTop() > 100 && !$("#navbar").hasClass("bg-mainBlue")) {
-        $("#navbar").addClass("bg-mainBlue");
+    if ($(document).scrollTop() > 100 && !$("#navbar").hasClass("scrolled-navbar")) {
+        $("#navbar").addClass("scrolled-navbar");
         console.log("xx");
     }
-    if ($(document).scrollTop() < 100 && $("#navbar").hasClass("bg-mainBlue")) {
-        $("#navbar").removeClass("bg-mainBlue");
+    if ($(document).scrollTop() < 100 && $("#navbar").hasClass("scrolled-navbar")) {
+        $("#navbar").removeClass("scrolled-navbar");
         console.log("ee");
     }
 })
